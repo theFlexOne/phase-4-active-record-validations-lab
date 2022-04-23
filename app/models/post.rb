3366f37ffc4e6957c28340904bde06b365721452
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-04-23 02:59:55 -0500

    complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait
+
+  private
+
+  def clickbait
+    if title == "True Facts"
+      errors.add(:title, "\"True Facts\" is not allowed")
+    end
+  end
 end
